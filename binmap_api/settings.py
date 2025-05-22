d62db346cb34bff14348acceb2641b55519d4214
ElGuill3 <guillermo.personal405875@gmail.com>
2025-05-22 02:00:40 -0600

    cambios

--- binmap_api/settings.py
+++ binmap_api/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = getenv('ALLOWED_HOSTS', '').split(',')
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
